474177f013d7dd696b64bb505a7c6dff47073f7c
RodrigoAlb <rodrigomedeiros4996@gmail.com>
2021-06-10 14:46:50 -0300

    v 1.0

---
+++ binario.js
@@ -0,0 +1,19 @@
+function binario (n) {
+    
+    let decomposicao = ''
+    for (let i = n ; i > 0 ; i = Math.floor(i / 2)) {
+        if(i % 2 == 0){
+            decomposicao += 0
+        }
+        else {
+            decomposicao += 1
+        }
+    }
+    let binario = ''
+    for (let i = decomposicao.length - 1; i >= 0; i--) {
+        binario += decomposicao[i]
+    }
+    console.log(binario)
+}
+
+binario(37)
